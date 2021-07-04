7f140db2c01437e1e0c794f343588d74d2ab4fd7
Kuvimello <77801535+Kuvimello@users.noreply.github.com>
2021-07-04 15:10:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function cache() {
+    console.log(1+1)
+    console.log(1+43864823648)
+    console.log(49037865860948609*583497547347598347)
+    console.log("Hello!")
+    console.log("My first console project")
+}
